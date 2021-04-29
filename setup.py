7f9bd652fac64772444327cbb988db56ce11a516
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-04-29 14:09:56 +0430

    updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='mehrshad-pypi',
+    packages=['mehrshad-pypi', 'tqdm', 'openpyxl', 'pathlib', 'pygame'],
+    version='0.1-beta',
+    description='description',
+    author='Mehrshad Dadashzadeh',
+    author_email='mehrdadashzadeh1379@gmail.com',
+    url='https://github.com/mehrshaad/mehrshad-pypi',
+    download_url='https://github.com/mehrshaad/mehrshad-pypi/archive/refs/tags/v0.1-beta.tar.gz',
+    keywords=['json', 'excel','pygame','mehrshad'],
+    classifiers=[],
+)
