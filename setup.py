7f5820d0b5c00e2e7a61bc636eae862f62b3a2c3
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2021-04-29 22:21:37 +0430

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='mehrshad',
     packages=['mehrshad'],
-    version='0.2-beta',
+    version='0.3-beta',
     description='A Python 3 module that contains all of my collections to let you code easier!',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     author='Mehrshad Dadashzadeh',
     author_email='mehrdadashzadeh1379@gmail.com',
     url='https://github.com/mehrshaad/mehrshad-pypi',
@@ -17,5 +23,4 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
     ],
-    python_requires='>=3.4',
 )
